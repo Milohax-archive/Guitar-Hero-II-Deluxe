e67ad25612913ad1ea34155b4c335fda536cd970
jnack <55568980+jnackmclain@users.noreply.github.com>
2022-12-07 17:32:40 -0600

    build test

---
+++ dependencies/download_gh1_songs.py
@@ -0,0 +1,43 @@
+# enable_keys.py
+from pathlib import Path
+import shutil
+import os
+try:
+    import git
+    print("module 'git' is installed. Downloading GH1DX songs repo & enabling GH1DX songs for Xenia, this may take some time.")
+except ModuleNotFoundError:
+    print("module 'git' is not installed. Install it via '/dependencies/install_gitpython.bat' or 'pip install gitpython'")
+    sys.exit(1)
+    
+# get the current working directory
+cwd = Path().absolute()
+
+# clone/pull gh2dx songs
+gh2dx_songs_source_path = cwd.joinpath("gh1dx-songs")
+
+if gh2dx_songs_source_path.exists():
+    # folder exists, pull the repository
+    repo = git.Repo(gh2dx_songs_source_path)
+    origin = repo.remotes.origin
+    origin.pull()
+else:
+    # folder does not exist, clone the repository
+    repo = git.Repo.clone_from("https://github.com/hmxmilohax/gh1dx-songs.git", gh2dx_songs_source_path, branch="main")
+
+gh2dx_songs_source_folder = cwd.joinpath("gh1dx-songs/GHIDX/songs")
+gh2dx_customs_folder = cwd.joinpath("_ark/songs")
+files = os.listdir(gh2dx_songs_source_path)
+
+# Check if the destination folder exists
+if os.path.exists(gh2dx_customs_folder):
+    # If it exists, remove it
+    shutil.rmtree(gh2dx_customs_folder)
+
+# Copy the source folder to the destination
+shutil.copytree(gh2dx_songs_source_folder, gh2dx_customs_folder, ignore_dangling_symlinks=False)
+
+# Delete all .vgs files
+for path in gh2dx_customs_folder.glob('**/*.mogg'):
+    os.remove(path)
+
+print(f"Successfully downloaded GH1DX songs. They will be available the next time you play.")
